2c934b4030de2d76219d5fa5077a7f2c0a5aee80
Kresimir I. Nurnberger <kin9006@aol.com>
2016-01-18 11:52:42 +0100

    final draft

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-
+var randNum;
+randomNumber();
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,63 +13,65 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  		var randNum = randomNumber();
   		var i = 0
 	$('#guessButton').click(function(event) {
 
 	  	console.log(randNum);
-
 	  	var userNum = $('#userGuess').val();
-	  	// var arizonaHot = (randNum - 10) < (randNum) < (randNum + 10);
-	  	if (randNum == userNum) {
-	  		$('#feedback').text('You Won. Click new game to play again');
-	  	}
-	  	// else if (userNum == arizonaHot){
-	  	// 	$('#feedback').text('Arizona Hot!');
-	  	// }
-	  	else if (userNum < randNum) {
-	   		$('#feedback').text('Too low.');
-	  	}
-	  	else if (userNum > randNum) {
-	  		$('#feedback').text('Too high.');
-	  	}
-	  	
-	  	else if (userNum == userNum.val('')) {
-	  		alert('Please enter a number from 1 to 100');
-	  	}
 
-	  	i++
-	  	$('#count').text(i);
+	  	var hot = Math.abs(randNum - userNum) >= 10 && Math.abs(randNum - userNum) < 20; 
+	  	var warm = Math.abs(randNum - userNum) >= 20 && Math.abs(randNum - userNum) < 30; 
+	  	var veryHot = Math.abs(randNum - userNum) < 10;
+	  	var iceCold = Math.abs(randNum - userNum) >= 50;
+	  	var cold = Math.abs(randNum - userNum) >= 30 && Math.abs(randNum - userNum) < 50;
 
-	  	$('#guessList').append('<li>'+userNum+'</li>');
+	  	if (userNum < 0 || userNum > 101 || userNum == "") {
+  			$('#feedback').text('Please choose a number between 0 and 100');	
+  		} else {
 
-		$('.text').val('');
+		  	if (randNum == userNum) {
+		  		$('#feedback').text('You Won. Click new game to play again');
+		  	}
+		  	else if (veryHot) {
+		  		$('#feedback').text('Pheonix hot!');
+		  	}
+		  	else if (iceCold) {
+		  		$('#feedback').text('You are in Antarctica!');
+		  	}
+		  	else if (cold) {
+		  		$('#feedback').text('Cold!');
+		  	}
+		  	else if (warm) {
+		  		$('#feedback').text('L.A. warm!');
+		  	}
+			else if (hot) {
+		  		$('#feedback').text('Florida hot!');
+		  	}
+
+		  	i++
+		  	$('#count').text(i);
+
+		  	$('#guessList').append('<li>'+userNum+'</li>');
+
+			$('.text').val('');
+		}
 
 		event.preventDefault();
 
 	})
-	$('.new').click(function(event) {
 
+	$('.new').click(function(event) {
 			i = 0;
 			$('#feedback').text('Make your Guees!');
-			$('#guessList').empty();
+			$('#guessList, .text').empty();
 			$('#count').text(i);
 
+			randomNumber();
+
 			event.preventDefault();
-			return randomNumber();
 	})
 });
 
 function randomNumber() {
-	return Math.floor(Math.random() * 100);
+	randNum = Math.floor(Math.random() * 100)+1;
 }
-// function newGame() {
-// 	$('.new').click(function(event) {
-// 		i = 0;
-// 		$('#feedback').text('Make your Guees!');
-// 		$('#guessList').empty();
-// 		$('#count').text(i);
-// 		return randomNumber();
-// 		event.preventDefault();
-// 	})
-// }
